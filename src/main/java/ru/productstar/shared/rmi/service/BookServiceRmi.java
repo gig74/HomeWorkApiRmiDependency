e55e48a811e2f161931a6428d3d0cff44868154b
gig74 <gig74_tmp@mail.ru>
2026-05-17 14:24:06 +0500

    02 - add "RemoteException"

--- src/main/java/ru/productstar/shared/rmi/service/BookServiceRmi.java
+++ src/main/java/ru/productstar/shared/rmi/service/BookServiceRmi.java
@@ -3,14 +3,15 @@ package ru.productstar.shared.rmi.service;
 import ru.productstar.shared.rmi.dto.BookRmi;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface BookServiceRmi extends Remote {
-    BookRmi createBook(BookRmi book);
-    List<BookRmi> getAllBooks();
-    BookRmi getBookById(Long id);
-    List<BookRmi> searchBooks(String inventoryNumberPattern, String titlePattern, String authorPattern);
-    BookRmi updateBook(Long id, BookRmi bookDetails);
-    void deleteBook(Long id);
-    long getBookCount();
+    BookRmi createBook(BookRmi book) throws RemoteException;
+    List<BookRmi> getAllBooks() throws RemoteException;
+    BookRmi getBookById(Long id) throws RemoteException;
+    List<BookRmi> searchBooks(String inventoryNumberPattern, String titlePattern, String authorPattern) throws RemoteException;
+    BookRmi updateBook(Long id, BookRmi bookDetails) throws RemoteException;
+    void deleteBook(Long id) throws RemoteException;
+    long getBookCount() throws RemoteException;
 }
